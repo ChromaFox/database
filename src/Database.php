66fa34bdd690eda724843b7a967287cce9a47cb7
Socks the Fox <jade@socksthefox.net>
2018-09-20 12:24:51 -0500

    Beginnings of Query Builder system

---
+++ src/Database.php
@@ -0,0 +1,107 @@
+<?php namespace Minifox;
+
+class Database
+{
+	private $db = null;
+	private $queryList = array();
+	private $prefixes = [];
+	
+	private function recordQuery($sql, $values, $time)
+	{
+		if($values != null)
+		{
+			if(strpos($sql, '?') !== false)
+			{
+				$pos = 0;
+				foreach($values as $v)
+				{
+					$pos = strpos($sql, '?', $pos);
+					if($pos !== false)
+						$sql = substr_replace($sql, $v, $pos, 1);
+					$pos += strlen($v);
+				}
+			}
+			else
+			{
+				foreach($values as $k => $v)
+					$sql = str_replace($k, $v, $sql);
+			}
+		}
+		
+		$this->queryList[] = array('time' => $time, 'sql' => $sql, 'prettytime' => round($time * 1000, 2) . 'ms');
+	}
+	
+	public function __construct($vendor, $dbname, $host, $user, $pass, $charset = "utf8", $persistent = true)
+	{
+		$start = microtime(true);
+		$this->db = new \PDO($vendor.":dbname=".$dbname.";host=".$host.";charset=".$charset, $user, $pass, [\PDO::ATTR_PERSISTENT => $persistent]);
+		$this->recordQuery("CONNECT", [], microtime(true) - $start);
+		
+		$this->prefixes[''] = "";
+	}
+	
+	public function setNamedPrefix($via, $prefix)
+	{
+		$this->prefixes[$via] = $prefix;
+	}
+	
+	public function setNamedPrefixList($list)
+	{
+		$this->prefixes = array_merge($this->prefixes, $list);
+	}
+	
+	public function getQueries()
+	{
+		return $this->queryList;
+	}
+
+	public function run($sql, $values = null)
+	{
+		$start = microtime(true);
+		
+		$query = $this->db->prepare($sql);
+		$query->execute($values);
+		$err = $query->errorInfo();
+		if($err[0] != 0)
+		{
+			print_r($err);
+			print_r(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
+		}
+		$results = $query->fetchAll();
+		$query->closeCursor();
+		
+		$this->recordQuery($sql, $values, microtime(true) - $start);
+		
+		return $results;
+	}
+	
+	public function lastInsertId()
+	{
+		return $this->db->lastInsertId();
+	}
+	
+	public function isConnected()
+	{
+		return $this->db != null;
+	}
+	
+	public function beginTransaction()
+	{
+		return $this->db->beginTransaction();
+	}
+	
+	public function commitTransaction()
+	{
+		return $this->db->commit();
+	}
+	
+	public function rollbackTransaction()
+	{
+		return $this->db->rollBack();
+	}
+	
+	public function query($prefix = "")
+	{
+		return new \Minifox\Database\Query($this, $this->prefixes[$prefix]);
+	}
+}
---
+++ src/Database/Query.php
@@ -0,0 +1,229 @@
+<?php namespace Minifox\Database;
+
+class Query implements \IteratorAggregate
+{
+	private $db = null;
+	
+	private $action = null;
+	private $table = null;
+	private $prefix = "";
+	private $columns = null;
+	
+	private $join = null;
+	
+	private $whereValues = null;
+	private $orderByValues = null;
+	private $limitValues = null;
+	
+	private $results = null;
+	
+	public function __construct(\Minifox\Database $db, $tablePrefix = "")
+	{
+		$this->db = $db;
+		$this->prefix = $tablePrefix;
+	}
+	
+	public function getIterator()
+	{
+		if($this->results == null)
+			$this->run();
+		
+		return new ArrayIterator($this->results);
+	}
+	
+	public function run()
+	{
+		$result = null;
+		
+		$query = call_user_func([$this, 'format'.$this->action]);
+		
+		if($this->whereValues)
+		{
+			$where = Query::formatWhere($this->whereValues);
+			$query['sql'] .= " WHERE " . $where['sql'];
+			$query['values'] = array_merge($query['values'], $where['values']);
+		}
+		
+		if($this->orderByValues)
+			$query['sql'] .= " ORDER BY " . Query::formatOrderBy($this->orderByValues);
+		
+		if($this->limitValues)
+			$query['sql'] .= " LIMIT " . Query::formatLimit($this->limitValues);
+		
+		$result = $this->db->run($query['sql'], $query['values']);
+		
+		$this->results = $result;
+		return $result;
+	}
+	
+	public function select($table, $columns = "*")
+	{
+		$this->action = "Select";
+		$this->table = $table;
+		$this->columns = $columns;
+		
+		return $this;
+	}
+	
+	public function leftJoin($table, $on)
+	{
+		if(!is_array($this->join))
+			$this->join = [];
+		
+		if(!is_array($this->join['LEFT']))
+			$this->join['LEFT'] = [];
+		
+		$this->join['LEFT'][$table] = $on;
+	}
+	
+	public function where($values)
+	{
+		if(is_array($this->whereValues))
+			$this->whereValues = array_merge_recursive($this->whereValues, $values);
+		else
+			$this->whereValues = $values;
+		
+		return $this;
+	}
+	
+	public function andWhere($values)
+	{
+		return $this->where($values);
+	}
+	
+	public function orWhere($values)
+	{
+		$this->whereValues = ['OR' => array_merge_recursive($this->whereValues, $values)];
+		
+		return $this;
+	}
+	
+	public function orderBy($values)
+	{
+		if(is_array($this->orderByValues))
+			$this->orderByValues = array_merge($this->orderByValues, $values);
+		else
+			$this->orderByValues = $values;
+		
+		return $this;
+	}
+	
+	public function limit($count, $start = null)
+	{
+		if($start !== null)
+			$this->limitValues = [$count, $start];
+		else
+			$this->limitValues = $count;
+		
+		return $this;
+	}
+	
+	public function page($page, $pageSize)
+	{
+		$pageStart = ($page - 1) * $pageSize;
+		if($pageSize > 0 && $pageStart >= 0)
+			$this->limit($pageSize, $pageStart);
+		
+		return $this;
+	}
+	
+	private function formatSelect()
+	{
+		if(is_array($this->columns))
+			$formattedCols = implode(", ", $this->columns);
+		else
+			$formattedCols = $this->columns;
+		
+		if(is_array($this->join))
+		{
+			// Join-y fun!
+			$sql = "SELECT {$formattedCols} FROM {$this->prefix}{$this->table}";
+			if(isset($this->join["LEFT"]))
+			{
+				$left = $this->join["LEFT"];
+				foreach($left as $leftTable => $on)
+					$sql .= " LEFT JOIN {$this->prefix}{$leftTable} ON {$on}";
+			}
+		}
+		else
+			$sql = "SELECT {$formattedCols} FROM {$this->prefix}{$this->table}";
+		
+		return ['sql' => $sql, 'values' => []];
+	}
+	
+	private static function formatWhere($where, $combine = "AND")
+	{
+		$sql = "";
+		
+		$values = [];
+		
+		$clauses = [];
+		
+		foreach($where as $col => $value)
+		{
+			// remove comment/ID/uniquifier from column
+			$commentPos = strpos($col, '#');
+			if($commentPos !== false)
+				$col = substr($col, 0, $commentPos);
+			
+			if(in_array($col, ["AND", "OR"]))
+			{
+				// Sub-where
+				$subWhere = Query::formatWhere($value, $col);
+				
+				$clauses[] = "({$subWhere['sql']})";
+				$values = array_merge($values, $subWhere['values']);
+			}
+			else
+			{
+				if(is_array($value))
+				{
+					$value = array_unique($value);
+					if(count($value) == 1)
+						$value = $value[0];
+				}
+				
+				$colVals = explode(' ', $col);
+				$col = $colVals[0];
+				if(isset($colVals[1]))
+					$op = $colVals[1];
+				else if(is_array($value))
+					$op = "in";
+				else
+					$op = "=";
+				
+				if(is_array($value))
+				{
+					$values = array_merge($values, $value);
+					$clauses[] = "{$col} {$op} (" .  implode(',', array_fill(0, count($value), '?')) . ")";
+				}
+				else
+				{
+					$values[] = $value;
+					$clauses[] = "{$col} {$op} ?";
+				}
+			}
+		}
+		
+		$sql = implode(" {$combine} ", $clauses);
+		
+		return ['sql' => $sql, 'values' => $values];
+	}
+	
+	private static function formatOrderBy($columns)
+	{
+		$orders = [];
+		foreach($columns as $col => $dir)
+			$orders[] = "{$col} {$dir}";
+		
+		return implode(", ", $orders);
+	}
+	
+	private static function formatLimit($limit)
+	{
+		if(is_array($limit))
+			return "{$limit[0]}, {$limit[1]}";
+		else
+			return "{$limit}";
+	}
+}
